cad855c7ef03e42be28523a23a0338f785cbc828
yutao <642231346@qq.com>
2022-10-17 20:06:26 +0800

    发布

--- src/router/index.js
+++ src/router/index.js
@@ -191,7 +191,7 @@ Router.prototype.push = function push(location) {
 }
 
 export default new Router({
-  mode: 'history', // 去掉url中的#
+  // mode: 'history', // 去掉url中的#
   scrollBehavior: () => ( { y: 0 } ),
   routes: constantRoutes
 })
